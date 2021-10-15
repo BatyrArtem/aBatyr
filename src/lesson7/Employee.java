cd9672bce46a91f423b77793fcce777c770b2b39
Artem Batyr <artembatyr88@gmail.com>
2021-10-15 13:46:32 +0300

    hw 6

---
+++ src/lesson7/Employee.java
@@ -0,0 +1,5 @@
+package lesson7;
+
+public class Employee {
+//    static Department convertToDepartment(String s)
+}
---
+++ src/lesson7/InnerJoinDemo.java
@@ -0,0 +1,15 @@
+package lesson7;
+
+public class InnerJoinDemo {
+//    public static void main(String[] args) {
+//        System.out.println(convertToDepartment("3;Finance"));
+//    }
+//    static Department convertToDepartment(String str) {
+//        String[] arr = str.split(";");
+//        Department result = new Department(Integer.parseInt((arr[0]), arr[1]));
+//        return result;
+//    }
+//    static Employee convertToEmployee(String str) {
+//        return null;
+//    }
+}
