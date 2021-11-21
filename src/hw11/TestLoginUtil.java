ee6150c733be3791b2d5cb0c13cf5ebb4df9a760
Artem Batyr <artembatyr88@gmail.com>
2021-11-21 12:47:47 +0200

    hw11

---
+++ src/hw11/TestLoginUtil.java
@@ -0,0 +1,10 @@
+package hw11;
+
+import static hw11.LoginUtil.isUserAuthentic;
+
+public class TestLoginUtil {
+    public static void main(String[] args) {
+        boolean user1 = isUserAuthentic("login1_", "password1_", "password1_");
+        System.out.println(user1);
+    }
+}
